a6396a8416da270caef2facccbe1ab63a8c437d4
Edgar <git@edgarluque.com>
2018-11-13 14:02:36 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='teelib',
+    version='0.0.1',
+    packages=['teelib'],
+    url='https://github.com/Ryozuki/teelib',
+    license='MIT',
+    author='Ryozuki',
+    author_email='ryo@teeland.net',
+    description='A teeworlds library for python.'
+)
