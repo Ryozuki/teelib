d3964b9b2203584ca87ebcd663eeed038d39f9d3
Edgar <git@edgarluque.com>
2019-04-26 15:59:45 +0200

    upd

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     url='https://github.com/Ryozuki/teelib',
     license='MIT',
     author='Ryozuki',
-    author_email='ryo@teeland.net',
+    author_email='ryo@ryozuki.xyz',
     description='A teeworlds library for python.'
 )
