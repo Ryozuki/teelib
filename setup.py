4ac4e82f440db706cff788d26eed8b465a489ced
Edgar <git@edgarluque.com>
2020-12-14 19:03:05 +0100

    update credits

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='teelib',
     version='0.0.1',
     packages=['teelib'],
-    url='https://github.com/Ryozuki/teelib',
+    url='https://github.com/edg-l/teelib',
     license='MIT',
-    author='Ryozuki',
-    author_email='ryo@ryozuki.xyz',
+    author='Edgar',
+    author_email='git@edgarluque.com',
     description='A teeworlds library for python.'
 )
